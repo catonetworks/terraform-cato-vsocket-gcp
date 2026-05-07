d36ff2222bbd4d4e9cdc25beaca2aecb486251ab
Gerardo Reyes <gerardo.reyes@catonetworks.com>
2026-05-07 17:15:01 +0200

    update cato provider version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     cato = {
       source  = "catonetworks/cato"
-      version = ">= 0.0.70"
+      version = ">= 0.0.73"
     }
   }
   required_version = ">= 1.5"
